fa8004089948d9da0ae3a361b00849879abb4a5b
Abdullah Al Noman <nomansasc11@gmail.com>
2020-07-18 17:09:53 +0600

    Php Update

--- chapter 2/api.php
+++ chapter 2/api.php
@@ -1,12 +1,13 @@
 <?php
+ 
+ try{
+    $client = new SoapClient('https://footballpool.dataaccess.eu/info.wso?WSDL');
+    $result = $client->TopGoalScorers(
+        ['iTopN' => 1]
+    );
+ }catch(SoapClient $e){
 
-function wpb_admin_account(){
-$user = 'Username';
-$pass = 'Password';
-$email = 'email@domain.com';
-if ( !username_exists( $user )  && !email_exists( $email ) ) {
-$user_id = wp_create_user( $user, $pass, $email );
-$user = new WP_User( $user_id );
-$user->set_role( 'administrator' );
-} }
-add_action('init','wpb_admin_account');
+ }
+
+ $topScorer = $result->TopGoalScorersResult->tTopGoalScorer;
+ echo $topScorer->sName;
